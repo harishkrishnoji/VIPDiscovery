7066da599ed6838313c9c8a06333a8a392a9f151
Harish Krishnoji - 10105547 <harish.krishnoji@fiserv.com>
2021-10-20 03:30:26 +0000

    Debug OFF

--- lb_info/helper/script_conf.py
+++ lb_info/helper/script_conf.py
@@ -4,8 +4,8 @@ import logging
 def LOG(name="OFD_NS_VIP"):
     logging.basicConfig(format="%(asctime)s %(levelname)s | %(module)s | %(message)s", datefmt="%Y/%m/%d %H:%M:%S")
     log = logging.getLogger(name)
-    log.setLevel(logging.DEBUG)
-    # log.setLevel(logging.INFO)
+    # log.setLevel(logging.DEBUG)
+    log.setLevel(logging.INFO)
     return log
 
 
