c0f37b56114f4df72f6076fe188c6f1a3324051d
Harish Krishnoji - 10105547 <harish.krishnoji@fiserv.com>
2022-02-26 13:04:47 -0500

    add log to delete method

--- lb_vserver/nautobot/nautobot_vip_delete.py
+++ lb_vserver/nautobot/nautobot_vip_delete.py
@@ -1,7 +1,7 @@
 # pylint: disable=W1203, C0103, W0631, W0703
 """Nautobot REST API SDK."""
 
-
+from helper.local_helper import log
 from nautobot.nautobot_attr import VIPT_ATTR
 
 
@@ -10,6 +10,7 @@ class LB_VIP_DELETE(VIPT_ATTR):
 
     def vip_delete(self):
         vips = VIPT_ATTR.vip_attr.all()
+        log.info(f"Deleting VIPs [{len(vips)}]...")
         for vip in vips:
             vip.delete()
         self.pool_delete()
@@ -20,43 +21,52 @@ class LB_VIP_DELETE(VIPT_ATTR):
         self.certificates_delete()
         self.issuer_delete()
         self.organization_delete()
+        log.info("Job done")
 
     def pool_delete(self):
         pools = VIPT_ATTR.pools_attr.all()
+        log.info(f"Deleting Pools [{len(pools)}]...")
         for pool in pools:
             pool.delete()
 
     def members_delete(self):
         members = VIPT_ATTR.members_attr.all()
+        log.info(f"Deleting Pool Members [{len(members)}]...")
         for member in members:
             member.delete()
 
     def policies_delete(self):
         policies = VIPT_ATTR.policies_attr.all()
+        log.info(f"Deleting Policies [{len(policies)}]...")
         for policy in policies:
             policy.delete()
 
     def partitions_delete(self):
         partitions = VIPT_ATTR.partitions_attr.all()
+        log.info(f"Deleting Partitions [{len(partitions)}]...")
         for partition in partitions:
             partition.delete()
 
     def certificates_delete(self):
         certificates = VIPT_ATTR.certificates_attr.all()
+        log.info(f"Deleting Certificates [{len(certificates)}]...")
         for certificate in certificates:
             certificate.delete()
 
     def environments_delete(self):
         environments = VIPT_ATTR.environments_attr.all()
+        log.info(f"Deleting Environments [{len(environments)}]...")
         for environment in environments:
             environment.delete()
 
     def issuer_delete(self):
         issuers = VIPT_ATTR.issuer_attr.all()
+        log.info(f"Deleting Issuers [{len(issuers)}]...")
         for issuer in issuers:
             issuer.delete()
 
     def organization_delete(self):
         organizations = VIPT_ATTR.organization_attr.all()
+        log.info(f"Deleting Organizations [{len(organizations)}]...")
         for organization in organizations:
             organization.delete()
